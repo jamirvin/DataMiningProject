efc51de28fde50cfea9564db934fc3d8465d8073
Corey Hyde <hyde.corey@gmail.com>
2016-04-27 17:39:33 -0400

    Changes, including Naive Bayes

--- import_data.py
+++ import_data.py
@@ -1,9 +1,12 @@
 import sys
+from datetime import datetime
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.cross_validation import cross_val_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import OneHotEncoder
 
 
 class Categorize:
@@ -37,14 +40,31 @@ def age_in_years(age):
     else:
         return 0
 
+def get_month(time):
+    dt = datetime.strptime(str(time), "%Y-%m-%d %H:%M:%S")
+    month = dt.month
+    return month
+
+def get_weekday(time):
+    dt = datetime.strptime(str(time), "%Y-%m-%d %H:%M:%S")
+    weekday = dt.weekday()
+    return weekday
+
+def get_hour(time):
+    dt = datetime.strptime(str(time), "%Y-%m-%d %H:%M:%S")
+    hour = dt.hour
+    return hour
 
 def import_array(filename: str):
     cats = Categorize()
 
     animals = pd.read_csv(filename, index_col=0)
     animals["AgeuponOutcome"] = animals.AgeuponOutcome.apply(age_in_years)
-    animals["AnimalType"] = animals.AnimalType.apply(cats.enumerate)
-    animals["SexuponOutcome"] = animals.SexuponOutcome.apply(cats.enumerate)
+    animals["AnimalType"] = pd.get_dummies(animals.AnimalType)
+    animals["SexuponOutcome"] = pd.get_dummies(animals.SexuponOutcome)
+    # animals["Month"] = animals.DateTime.apply(get_month)
+    # animals["Weekday"] = animals.DateTime.apply(get_weekday)
+    # animals["Hour"] = animals.DateTime.apply(get_hour)
     outcomes = animals.OutcomeType
 
     animals.drop("OutcomeType", 1, inplace=True)
@@ -53,14 +73,20 @@ def import_array(filename: str):
     return animals, outcomes
 
 
-def first_pass(animals, outcomes) -> RandomForestClassifier:
+def first_pass(animals, outcomes):# -> RandomForestClassifier:
     animals.drop(["Breed", "Color", "DateTime", "Name", "OutcomeSubtype"], 1, inplace=1)
     forrest = RandomForestClassifier(n_jobs=-1, n_estimators=100)
-    extra = ExtraTreesClassifier(n_jobs=-1, criterion="entropy")
+    extra = ExtraTreesClassifier(n_jobs=-1, criterion="entropy");
 
     score = cross_val_score(extra, animals, outcomes, n_jobs=-1, cv=5)
     print("\n", np.mean(score))
     return forrest
+
+def naive(animals, outcomes):
+    gnb = GaussianNB()
+    gnb.fit(animals, outcomes)
+    score = cross_val_score(gnb, animals, outcomes, n_jobs=-1)
+    print("\n", np.mean(score))
 
 
 def main(argv):
@@ -70,6 +96,7 @@ def main(argv):
 
     animals, outcomes = import_array(argv[1])
     forrest = first_pass(animals, outcomes)
+    naive(animals, outcomes)
 
 
 if __name__ == "__main__":
